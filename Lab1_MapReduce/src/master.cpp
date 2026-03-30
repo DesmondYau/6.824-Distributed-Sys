4269d8e5437643caca7cdd633b71b8254e27afa3
Desmond <desmondyautsinwai@gmail.com>
2026-03-30 09:44:06 +0800

    Progress on lab 1, producing intermediate result

--- Lab1_MapReduce/src/master.cpp
+++ Lab1_MapReduce/src/master.cpp
@@ -1,65 +1,163 @@
 #include <iostream>
-#include <vector>
-#include "./buttonrpc-master/buttonrpc.hpp"
+#include <queue>
+#include <chrono>
+#include "../include/buttonrpc-master/buttonrpc.hpp"
+
+const int timeoutSec { 10 };
+
+enum class TaskState { 
+    unassigned, 
+    assigned, 
+    completed 
+};
+
+enum class TaskType { 
+    mapTask, 
+    reduceTask, 
+    emptyTask, 
+    completeTask 
+};
+
+struct Task
+{
+    int taskID_;
+    std::string fileName_;
+    TaskType taskType_;
+    TaskState taskState;
+    std::chrono::steady_clock::time_point deadline;
+
+    // Implementation for buttonrpc
+    friend Serializer& operator >> (Serializer& in, Task& d) {
+		in >> d.taskID_ >> d.fileName_ >> d.taskType_ >> d.taskState >> d.deadline;
+		return in;
+	}
+
+    // Implementation for buttonrpc
+	friend Serializer& operator << (Serializer& out, Task& d) {
+		out << d.taskID_ << d.fileName_ << d.taskType_ << d.taskState << d.deadline;
+		return out;
+	}
+};
+using TaskPtr = std::unique_ptr<Task>;
+
 
 class Master
 {
 public:
-    Master(int mapCount, int reduceCount);
-    void getAllFiles(int argc, char* argv[]);
-    int getMapCount();
+    Master(int mapCount, int reduceCount, const std::vector<std::string>& fileNames);
     int getReduceCount();
-
-
+    Task requestTask();
+  
 private:
-    int mapCount_;                                // number of map tasks
-    int reduceCount_;                             // number of reduce tasks
-    std::vector<std::string> files_;              // vector storing all input files
-
+    std::queue<TaskPtr> unassignedMapTasks_;   
+    std::queue<TaskPtr> unassignedReduceTasks_;                  
+    std::queue<TaskPtr> assignedTasks_;                  
+    int mapRemaining_;
+    int reduceRemaining_;
+    int reduceCount_;
 };
 
-Master::Master(int mapCount, int reduceCount)
-    : mapCount_ { mapCount }
+Master::Master(int mapCount, int reduceCount, const std::vector<std::string>& fileNames)
+    : mapRemaining_ { mapCount }
+    , reduceRemaining_ { reduceCount }
     , reduceCount_ { reduceCount }
-{}
-
-void Master::getAllFiles(int argc, char* argv[])
 {
-    for (int i=2; i<argc; i++)
+    for (int i=0; i<mapCount; i++)
     {
-        files_.emplace_back(argv[i]);
+        TaskPtr taskPtr { std::make_unique<Task>(
+            i,
+            fileNames[i],
+            TaskType::mapTask,
+            TaskState::unassigned,
+            std::chrono::steady_clock::time_point {}
+        )};
+        
+        unassignedMapTasks_.push(std::move(taskPtr));
+    }
+
+    for (int i=0; i<reduceCount; i++)
+    {
+        TaskPtr taskPtr { std::make_unique<Task>(
+            i,
+            "",
+            TaskType::mapTask,
+            TaskState::unassigned,
+            std::chrono::steady_clock::time_point {}
+        )};
+        
+        unassignedReduceTasks_.push(std::move(taskPtr));
     }
 }
 
-int Master::getMapCount() {
-    return mapCount_;
+int Master::getReduceCount()
+{
+    return reduceCount_;
 }
 
-int Master::getReduceCount() {
-    return reduceCount_;
+Task Master::requestTask()
+{
+    if (mapRemaining_)
+    {
+        if (!unassignedMapTasks_.empty())
+        {
+            TaskPtr taskPtr = std::move(unassignedMapTasks_.front());
+            taskPtr->deadline = std::chrono::steady_clock::now() + std::chrono::seconds(timeoutSec);
+
+            Task task { *taskPtr };
+            assignedTasks_.push(std::move(taskPtr));
+
+            return task;
+        }
+        else
+            return Task{ -1, "", TaskType::emptyTask, TaskState::completed, std::chrono::steady_clock::time_point{} };
+    }
+    
+    if (reduceRemaining_)
+    {
+        if (!unassignedReduceTasks_.empty())
+        {
+            TaskPtr taskPtr = std::move(unassignedReduceTasks_.front());
+            taskPtr->deadline = std::chrono::steady_clock::now() + std::chrono::seconds(timeoutSec);
+            
+            Task task { *taskPtr };
+            assignedTasks_.push(std::move(taskPtr));
+
+            return task;
+        }
+        else
+            return Task{ -1, "", TaskType::emptyTask, TaskState::completed, std::chrono::steady_clock::time_point{} };
+    }
+
+    return Task{ -1, "", TaskType::completeTask, TaskState::completed, std::chrono::steady_clock::time_point{} };
 }
 
 
 int main(int argc, char* argv[])
 {
-    if (argc < 2)
+    if (argc < 3)
     {
-        std::cout << "Missing parameter! Input format is './master ../testfiles/pg*.txt'" << std::endl;
+        std::cout << "Missing parameter! Input format is './master <number of reduce tasks> ../testfiles/pg*.txt'" << std::endl;
         return 1;
     }
 
+    // Parse arguments
+    int mapCount { argc - 2 };
+    int reduceCount { std::stoi(argv[1])};
+    std::vector<std::string> fileNames;
+    for (int i=2; i<argc; i++) {
+        fileNames.push_back(argv[i]);
+    }
 
+    // Initialize master and RPC server
     buttonrpc server;
     server.as_server(5555);
+    Master master { mapCount, reduceCount, fileNames };   
+    server.bind("requestTask", &Master::requestTask, &master); 
+    server.bind("getReduceCount", &Master::getReduceCount, &master); 
 
-    Master master { 8, 12 };
-    master.getAllFiles(argc, argv);
-    server.bind("getMapCount", &Master::getMapCount, &master);
-    server.bind("getReduceCount", &Master::getReduceCount, &master);
-	
-    std::cout << "Running RPC server on: " << 5555 << std::endl;
+    // Run RPC server
+    std::cout << "Starting server on RPC server on port 5555" << std::endl;
     server.run();
+
     return 0;
-
-
 }
--- Lab1_MapReduce/src/worker.cpp
+++ Lab1_MapReduce/src/worker.cpp
@@ -1,12 +1,164 @@
+#include <iostream>
+#include <fstream>
+#include <dlfcn.h>
+#include "../include/buttonrpc-master/buttonrpc.hpp"
+#include "../include/json.hpp"
 
-
-class Worker
-{
-
+enum class TaskState { 
+    unassigned, 
+    assigned, 
+    completed 
 };
 
-int main()
+enum class TaskType { 
+    mapTask, 
+    reduceTask, 
+    emptyTask, 
+    completeTask 
+};
+
+struct Task
 {
+    int taskID_;
+    std::string fileName_;
+    TaskType taskType_;
+    TaskState taskState;
+    std::chrono::steady_clock::time_point deadline;
+
+    friend Serializer& operator >> (Serializer& in, Task& d) {
+		in >> d.taskID_ >> d.fileName_ >> d.taskType_ >> d.taskState >> d.deadline;
+		return in;
+	}
+
+	friend Serializer& operator << (Serializer& out, Task& d) {
+		out << d.taskID_ << d.fileName_ << d.taskType_ << d.taskState << d.deadline;
+		return out;
+	}
+};
+
+struct KeyValuePair
+{
+    std::string key_;
+    std::string value_;
+};
+
+using MapFunc = std::vector<KeyValuePair>(*)(const std::string& filename, const std::string& contents);
+using ReduceFunc = std::string(*)(const std::string& key, const std::vector<std::string> values);
+
+int ihash(const std::string& key) {
+    const uint32_t fnv_prime = 16777619u;
+    uint32_t hash = 2166136261u;  
+
+    for (unsigned char c : key) {
+        hash ^= c;
+        hash *= fnv_prime;
+    }
+
+    return static_cast<int>(hash & 0x7fffffff);
+}
+
+void writeMapOutput(std::vector<KeyValuePair> intermediate, int reduceCount, int taskID)
+{
+    std::vector<std::ofstream> outFiles(reduceCount);
+    for (int i=0; i<reduceCount; i++)
+    {
+        std::string fname { "mr-" + std::to_string(taskID) + "-" + std::to_string(i) };
+        outFiles[i].open(fname);
+        if (!outFiles[i])
+        {
+            std::cerr << "Cannot open " << fname << std::endl;
+        }
+    }
+
+    for (const auto& kv : intermediate)
+    {
+        int i = ihash(kv.key_) % reduceCount;
+        nlohmann::json j;
+        j["key"] = kv.key_;
+        j["value"] = kv.value_;
+        outFiles[i] << j.dump() << "\n";
+    }
+
+    for (auto& f : outFiles)
+    {
+        f.close();
+    }
+
+}
+
+void doMapTask(const std::string& filename, MapFunc mapFunc, int reduceCount, int taskID)
+{
+    std::ifstream inf { filename };
+    if (!inf)
+    {
+        std::cerr << "Cannot open file " << filename << std::endl;
+        return;
+    }
     
+    std::stringstream buffer {};
+    buffer << inf.rdbuf();
+    inf.close();
+
+    std::vector<KeyValuePair> intermediate {};
+    auto kva = mapFunc(filename, buffer.str());
+    for (const auto& kv : kva)
+        intermediate.push_back(kv);
+
+    writeMapOutput(intermediate, reduceCount, taskID);
+}
+
+
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2)
+    {
+        std::cout << "Usage: ./worker ../testfiles/xxx.so";
+        return 1;
+    }
+
+    // Load shared object file
+    void* handle = dlopen(argv[1], RTLD_LAZY);
+    if (!handle)
+    {
+        std::cout << "Cannot load shared object file " << argv[1] << std::endl;
+        return 1;
+    }
+
+    // Obtain pointer to Map function in shared object file
+    MapFunc mapFunc = (MapFunc)dlsym(handle, "Map");
+    if (!mapFunc)
+    {
+        std::cout << "Cannot find Map function in shared object file" << std::endl;
+        return 1;
+    }
+
+    // Obtain pointer to Reduce function in shared object file
+    ReduceFunc reduceFunc = (ReduceFunc)dlsym(handle, "Reduce");
+    if (!reduceFunc)
+    {
+        std::cout << "Cannot find Reduce function in shared object file" << std::endl;
+        return 1;
+    }
+
+    buttonrpc client;
+	client.as_client("127.0.0.1", 5555);
+
+
+    Task task = client.call<Task>("requestTask").val();
+    int reduceCount = client.call<int>("getReduceCount").val();
+    if (task.taskType_ == TaskType::mapTask)
+    {
+        doMapTask(task.fileName_, mapFunc, reduceCount, task.taskID_);
+        // reportMapComplete(task.taskID_);
+    }
+    else if (task.taskType_ == TaskType::reduceTask)
+    {
+        // doReduceTask(task.taskID_, reduceFunc);
+        // reportReduceComplete(task.taskID_);
+    }
+    
+
     return 0;
+	
 }
